f415af3cdc103bfacbf595f2ed41af37b123444c
Minie <sayadethminie@gmail.com>
2024-07-11 12:28:46 -0400

    vercel bugs

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -14,7 +14,7 @@ function Thumbnail({ movie }: Props) {
           movie.backdrop_path || movie.poster_path
         }`}
         className="rounded-sm object-cover md:rounded"
-        layout="fill"
+        layout="fill" alt=""
       />
     </div>
   );
